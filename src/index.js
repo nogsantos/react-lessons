587b943dd3a52a5f65243e42efdbd4eabe2034b7
Fabricio Nogueira <nogsantos@gmail.com>
2018-02-09 23:12:23 -0200

    Some style

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './style.css'
 import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
     <BrowserRouter>
         <App />
     </BrowserRouter>
 ),document.getElementById('root'));
+
+registerServiceWorker();
